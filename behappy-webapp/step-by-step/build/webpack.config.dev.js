a58d59a676f6e73d91ebe938b26c0eac05bf4ac9
Allen <allen.gong@sap.com>
2018-08-07 09:39:42 +0800

    demo app for behappy frontend

---
+++ behappy-webapp/step-by-step/build/webpack.config.dev.js
@@ -0,0 +1,45 @@
+'use strict'
+
+const {HotModuleReplacementPlugin} = require('webpack')
+const {VueLoaderPlugin} = require('vue-loader')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: ['./src/app.js'],
+    devServer: {
+        hot: true,
+        watchOptions: {
+            poll: true
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
+            {
+                test: /\.styl(us)?$/,
+                use: [
+                  'vue-style-loader',
+                  'css-loader',
+                  'stylus-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                use: 'babel-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HotModuleReplacementPlugin(),
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html',
+            inject: true
+        })
+    ]
+}
