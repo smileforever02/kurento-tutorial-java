b582a29c1c804bc482805de9a751547140052135
Allen <allen.gong@sap.com>
2018-08-11 22:29:38 +0800

    add friend

--- behappy-webapp/step-by-step/src/pages/UserProfile.vue
+++ behappy-webapp/step-by-step/src/pages/UserProfile.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="full-width center-content">
-        <h1>Welcome, {{$route.params.userId}}!</h1>
+        <h1>Welcome, {{$route.params.userId}}!</h1><span class="glyphicon glyphicon-plus-sign add-friend"></span>
     </div>
 </template>
 
