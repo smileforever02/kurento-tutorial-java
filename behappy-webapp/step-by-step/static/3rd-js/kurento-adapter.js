6fa5817920d6b9a070954d9987b1d10881b1c58a
i311688 <allen.gong@sap.com>
2018-08-14 17:10:44 +0800

    layout adjust

--- behappy-webapp/step-by-step/static/3rd-js/kurento-adapter.js
+++ behappy-webapp/step-by-step/static/3rd-js/kurento-adapter.js
@@ -8,3 +8,16 @@ if(typeof incomingCall === 'function'){
         })
     }
 }
+
+//
+(function(){
+    let togger = $('.navbar-toggle[data-target="#bs-navbar"]');
+    let menu = $('#bs-navbar');
+    window.addEventListener('click', e =>{
+        if(e.target === togger[0]){
+            // console.log('togger clicked');
+        }else if(menu.hasClass('show')){
+            togger.trigger('click');
+        }
+    }, false);
+}())
