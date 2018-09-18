e88408bf0ba909395c09ba06e01291f884604a7d
i311688 <allen.gong@sap.com>
2018-09-18 14:00:12 +0800

    automatially copy files after build

--- behappy-webapp/step-by-step/build/copy-to-pro.js
+++ behappy-webapp/step-by-step/build/copy-to-pro.js
@@ -1,1 +1,13 @@
-console.log('start to copy to production folder')
+const copydir = require('copy-dir');
+const path = require('path');
+const fromDir = path.resolve(__dirname, '../dist');
+const toDir = path.resolve(__dirname, '../../../kurento-group-call/src/main/resources/static');
+
+console.log('start to copy to ' + toDir)
+copydir(fromDir, toDir, function(err){
+    if(err){
+        console.log(err);
+    } else {
+        console.log('copy done');
+    }
+});
