6ac77a6f7915b93b76f4e8256f13d105d4a81b84
Allen <allen.gong@sap.com>
2018-11-07 22:38:47 +0800

    replays UI

--- behappy-webapp/step-by-step/src/pages/Replays.vue
+++ behappy-webapp/step-by-step/src/pages/Replays.vue
@@ -1,12 +1,16 @@
 <template>
     <div class="full-width center-content">
-        <ul class="page-content item-list">
-            <li v-for="item in items" v-bind:key="item.userId" v-bind:data-userid="item.userId">
-                <img :src="item.photo">
-                <span v-on:click="checkUser(item.userId, item.displayName)">{{item.displayName}}</span>
-                <span v-on:click="callFriend(item.userId, item.displayName)" class="glyphicon glyphicon-facetime-video right" aria-hidden="true"></span>
+        <ul v-if="playing === false" class="page-content item-list">
+            <li v-for="item in items" v-bind:key="item.id" v-bind:data-id="item.id">
+                <span>{{item.time}}</span>
+                <span>{{item.label}}</span>
+                <span v-on:click="replay(item.id, item.src)" class="glyphicon glyphicon-expand right" aria-hidden="true"></span>
             </li>
         </ul>
+        <div v-if="playing === true" class="replay-wrapper">
+            <video id="replay-video" controls playsinline></video>
+            <span v-on:click="cancelReplay()" class="glyphicon glyphicon-remove-circle right" aria-hidden="true"></span>
+        </div>
     </div>
 </template>
 
@@ -21,11 +25,12 @@ console.log(routerGuard)
 const m = Object.assign({
     data(){
         return {
+            playing: false,
             items: []
         }
     },
     mounted(){
-        Services.getFriends().done((data) => {
+        Services.getReplays().done((data) => {
             // console.log(data);
             this.items = data.map(d => {
                 d.photo = d.photo || Services.getDefPhoto();
@@ -34,6 +39,20 @@ const m = Object.assign({
         }).fail(() => MessageBox.error('Sorry, can\'t find your friends'));
     },
     methods:{
+        replay(id, src){
+            this.playing = true;
+            this.$nextTick(() => {
+                let video = document.querySelector('#replay-video');
+                video.src = src;
+                video.play();
+            });
+        },
+        cancelReplay(){
+            let video = document.querySelector('#replay-video');
+            video.pause();
+            video.src = '';
+            this.playing = false;
+        },
         callFriend(userId, userName){
             Services.getUserStatus(userId).done(msg => {
                 if(msg.code === 0){
